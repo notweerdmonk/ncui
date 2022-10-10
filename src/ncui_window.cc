35f4dfbfe5e841e149f5d703e15b31f58c12aec8
notweerdmonk <wrdmnk@gmail.com>
2022-10-10 22:42:40 +0530

    Read character from window only if it has focus.

--- src/ncui_window.cc
+++ src/ncui_window.cc
@@ -47,8 +47,6 @@ class Window::WindowImpl {
 
   field_buf_t*   p_text_buf;
 
-  int            h, w, y, x;
-
   dim_t          win_dim;
   coord_t        win_coord;
   cursor_t       cur;
@@ -154,99 +152,103 @@ class Window::WindowImpl {
     win_event_t win_ev;
     int key;
 
-    win_ev = WIN_EV_NONE;
-    key = me.getchar();
+    if (me.has_focus) {
 
-    if (key == ERR) {
-      return NULL;
-    }
+      win_ev = WIN_EV_NONE;
+      key = me.getchar();
 
-    switch(key) {
-      case KEY_MOUSE:
-        {
-          if (getmouse(&ev) == OK) {
-            win_ev = WIN_EV_MOUSE;
-            /* copy data */
-            me.ev_lookup[win_ev].cb_data = &ev.bstate;
-          }
-          break;
-        }
-      case KEY_RESIZE:
-        {
-          if (me.was_resized == true) {
-            win_ev = WIN_EV_RESIZE;
-            me.ev_lookup[win_ev].cb_data = &me.resize_dim;
-          }
-          break;
-        }
-      default:
-        {
-          if (me.is_textfield) {
-            /* Backspace needs special handling */
-            if (key == KEY_BACKSPACE) {
-              win_ev = WIN_EV_TERM;
-              me.ev_lookup[win_ev].cb_data = &key;
-              me.bksp();
+      if (key == ERR) {
+        return NULL;
+      }
+
+      switch(key) {
+        case KEY_MOUSE:
+          {
+            if (getmouse(&ev) == OK) {
+              win_ev = WIN_EV_MOUSE;
+              /* copy data */
+              me.ev_lookup[win_ev].cb_data = &ev.bstate;
             }
-            /* Keyboard events */
-            else if ((key >= 0) &&
-                (key <= 127)) {
-              win_ev = WIN_EV_TERM;
-              me.ev_lookup[win_ev].cb_data = &key;
+            break;
+          }
+        case KEY_RESIZE:
+          {
+            if (me.was_resized == true) {
+              win_ev = WIN_EV_RESIZE;
+              me.ev_lookup[win_ev].cb_data = &me.resize_dim;
+            }
+            break;
+          }
+        default:
+          {
+            if (me.is_textfield) {
+              /* Backspace needs special handling */
+              if (key == KEY_BACKSPACE) {
+                win_ev = WIN_EV_TERM;
+                me.ev_lookup[win_ev].cb_data = &key;
+                me.bksp();
+              }
+              /* Keyboard events */
+              else if ((key >= 0) &&
+                  (key <= 127)) {
+                win_ev = WIN_EV_TERM;
+                me.ev_lookup[win_ev].cb_data = &key;
 
-              if (me.cur.x <= me.win_dim.w) {
-                if (key != 9) {
-                  me.addchar(key);
+                if (me.cur.x <= me.win_dim.w) {
+                  if (key != 9) {
+                    me.addchar(key);
+                  }
                 }
-              }
 
-              if (key == 10) {
-                me.p_text_buf->newline();
-              }
-              else {
-                if (key != 9) {
-                  me.p_text_buf->put(key);
-                }
-              }
-
-              if (me.is_bordered == true) {
-                /* Enter/Return key */
                 if (key == 10) {
-                  if (me.cur.y < me.win_dim.h) {
-                    me.move_cur(++me.cur.y, 1);
-                  } else if (me.cur.y == me.win_dim.h) {
-                    me.move_cur(me.cur.y, --me.cur.x);
-                  }
-                  me.box();
+                  me.p_text_buf->newline();
                 }
-                else if (me.cur.x > me.win_dim.w) {
-                  if (me.cur.y < me.win_dim.h) {
-                    me.move_cur(++me.cur.y, 1);
+                else {
+                  if (key != 9) {
+                    me.p_text_buf->put(key);
                   }
-                  else if (me.cur.y == me.win_dim.h) {
-                    if (me.is_textfield) {
-                      me.p_text_buf->move_col_rel(-1);
+                }
+
+                if (me.is_bordered == true) {
+                  /* Enter/Return key */
+                  if (key == 10) {
+                    if (me.cur.y < me.win_dim.h) {
+                      me.move_cur(++me.cur.y, 1);
+                    } else if (me.cur.y == me.win_dim.h) {
+                      me.move_cur(me.cur.y, --me.cur.x);
+                    }
+                    me.box();
+                  }
+                  else if (me.cur.x > me.win_dim.w) {
+                    if (me.cur.y < me.win_dim.h) {
+                      me.move_cur(++me.cur.y, 1);
+                    }
+                    else if (me.cur.y == me.win_dim.h) {
+                      if (me.is_textfield) {
+                        me.p_text_buf->move_col_rel(-1);
+                      }
                     }
                   }
                 }
+
               }
-
-            }
-            /* Movement keys */
-            else if ((key >= KEY_DOWN) &&
-                (key < KEY_MOUSE)) {
-              win_ev = WIN_EV_KEY;
-              me.ev_lookup[win_ev].cb_data = &key;
-            }
-          } /* if (is_textfield) */
-        }
-    }
-
-    if (win_ev != WIN_EV_NONE) {
-      win_ev_entry_t ev_data = me.ev_lookup[win_ev];
-      if (ev_data.cb != NULL) {
-        ev_data.cb(ev_data.cb_data, ev_data.user_data);
+              /* Movement keys */
+              else if ((key >= KEY_DOWN) &&
+                  (key < KEY_MOUSE)) {
+                win_ev = WIN_EV_KEY;
+                me.ev_lookup[win_ev].cb_data = &key;
+              }
+            } /* if (is_textfield) */
+          }
       }
+
+      if (win_ev != WIN_EV_NONE) {
+        win_ev_entry_t ev_data = me.ev_lookup[win_ev];
+        if (ev_data.cb != NULL) {
+          ev_data.cb(ev_data.cb_data, ev_data.user_data);
+        }
+      }
+
     }
 
     return NULL;
